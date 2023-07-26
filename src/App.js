83351ce15821ca30e4095af426ac127338cd55ba
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-07-26 23:05:36 +0530

    tempory changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Home from "./components/Home";
 // import Fakenav from "./TestComponents/Fakenav"
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +12,7 @@ function App(){
     return(
         <>
         <BrowserRouter>
+        
         
         <Navbar />
         <Routes>
@@ -23,6 +24,7 @@ function App(){
             <Route path="/projects" element={<Projects />}></Route>
         </Routes>
         </BrowserRouter>
+    
         </>
     )
 }
